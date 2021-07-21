74b46c7514c287340edc4436c645bc30c6b092d6
Hector <hectorcustodio@outlook.com.br>
2021-07-21 11:26:47 -0300

    Sign commit test

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -24,6 +24,7 @@ const CartDropdown = ({ cartItems, history, setVisibility }: WithRouterCartInter
         }
       </div>
       <FormButton onClick={() => {
+        
         history.push('/checkout')
         setVisibility()
       }}>GO TO CHECKOUT</FormButton>
