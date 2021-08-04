edb1e29bbad4e42566b5d2a150d9acabbb9181bf
Hector <hectorcustodio@outlook.com.br>
2021-08-04 15:35:23 -0300

    No GPG signed commit
    Signed-off-by: Hector <hectorcustodio@outlook.com.br>

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -12,6 +12,9 @@ const CartIcon = ({ setVisibility, itemCount }: CartIconInterface) => (
   <div className='cart-icon' onClick={setVisibility}>
     <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>{itemCount || 0}</span>
+
+
+    
   </div>
 )
 
