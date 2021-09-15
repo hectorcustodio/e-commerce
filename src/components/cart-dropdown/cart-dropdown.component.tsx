4b033fefe5235c03e0279659b9c8e51d0ef8a718
Hector <hectorcustodio@outlook.com.br>
2021-09-15 13:35:46 -0300

    Signed
    Signed-off-by: Hector <hectorcustodio@outlook.com.br>

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -18,6 +18,8 @@ const CartDropdown = ({ cartItems, history, setVisibility }: WithRouterCartInter
     <div className='cart-dropdown'>
       
       <div className='cart-items'>
+
+        
         {
           cartItems.length > 0 ?
             cartItems && cartItems.map((cartItem: Item) => <div key={cartItem.id}><CartItem  {...cartItem} /></div>) :
@@ -29,7 +31,7 @@ const CartDropdown = ({ cartItems, history, setVisibility }: WithRouterCartInter
 
         history.push('/checkout')
 
-        
+
         setVisibility()
       }}>GO TO CHECKOUT</FormButton>
       
